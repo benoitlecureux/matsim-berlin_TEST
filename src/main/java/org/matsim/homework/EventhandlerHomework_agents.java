85aacbfa8d1bd2b6f4dd025e63a315ecfbdec0ec
benoitlecureux <64031209+benoitlecureux@users.noreply.github.com>
2020-06-08 16:32:33 +0200

    New branch for homework 1

--- src/main/java/org/matsim/homework/EventhandlerHomework_agents.java
+++ src/main/java/org/matsim/homework/EventhandlerHomework_agents.java
@@ -4,6 +4,8 @@ import org.matsim.core.api.experimental.events.EventsManager;
 import org.matsim.core.events.EventsUtils;
 import org.matsim.core.events.MatsimEventsReader;
 
+import java.util.Set;
+
 
 public class EventhandlerHomework_agents {
 
@@ -11,15 +13,26 @@ public class EventhandlerHomework_agents {
 
         String inputFile = "C:/Users/Admin/Documents/Benoit/ENTPE/2A/JAVA/IntelliJ/matsim-berlin_TEST/OUPUTS/output-berlin-v5.2-1pct_V4_150it_1_LANE/berlin-v5.2-1pct.output_events.xml.gz";
 
-        EventsManager eventsManager = EventsUtils.createEventsManager();
+        EventsManager eventsManager1 = EventsUtils.createEventsManager();
 
-        SimpleEventHandler_agents eventHandler = new SimpleEventHandler_agents();
-        eventsManager.addHandler(eventHandler);
+        SimpleEventHandler_vehicles eventHandlerVehicles = new SimpleEventHandler_vehicles();
+        eventsManager1.addHandler(eventHandlerVehicles);
 
-        MatsimEventsReader eventsReader = new MatsimEventsReader(eventsManager);
-        eventsReader.readFile(inputFile);
+        MatsimEventsReader eventsReader1 = new MatsimEventsReader(eventsManager1);
+        eventsReader1.readFile(inputFile);
 
-        eventHandler.printVehicle();
+
+        Set<String> affectedVehicles = eventHandlerVehicles.getAffectedVehicles();
+
+
+        EventsManager eventsManager2 = EventsUtils.createEventsManager();
+
+        SimpleEventHandler_agents eventHandlerAgents = new SimpleEventHandler_agents();
+        eventsManager2.addHandler(eventHandlerAgents);
+
+        MatsimEventsReader eventsReader2 = new MatsimEventsReader(eventsManager2);
+        eventsReader2.readFile(inputFile);
+
 
     }
 }
