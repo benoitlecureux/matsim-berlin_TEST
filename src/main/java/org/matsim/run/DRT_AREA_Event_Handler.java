b8fbe9a84cca65e0561a88f04ba8dcd4d6310346
benoitlecureux <64031209+benoitlecureux@users.noreply.github.com>
2020-07-26 23:44:22 +0200

    Homework n°2 working

---
+++ src/main/java/org/matsim/run/DRT_AREA_Event_Handler.java
@@ -0,0 +1,23 @@
+package org.matsim.run;
+
+import org.geotools.data.FeatureEvent;
+import org.geotools.data.FeatureListener;
+import org.geotools.data.simple.SimpleFeatureSource;
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.network.Link;
+import org.matsim.core.utils.gis.ShapeFileReader;
+
+import java.util.Map;
+
+public class DRT_AREA_Event_Handler {
+    public static void main(String[] args) {/*
+        Scenario scenario = new Scenario;
+        Map<Id<Link>, Link> links = scenario.getNetwork().getLinks();
+        SimpleFeatureSource shpfile = ShapeFileReader.readDataFile("input/BerlinShapeFile.shp");
+        if (links : shpfile.getFeatures().features())
+
+        //FeatureListener featureListener = new Link().getCoord().getX();
+        //ShapeFileReader.readDataFile("input/BerlinShapeFile.shp").getSupportedHints().contains(Link);*/
+    }
+}
